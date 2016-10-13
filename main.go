d4e2e780260c70df485a666eff3254a3d33aaefc
sugyan <sugi1982@gmail.com>
2016-10-13 20:55:38 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 
-	"fmt"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/sugyan/face-manager-linebot/inferences"
 )
@@ -37,63 +38,68 @@ type app struct {
 func (a *app) handler(w http.ResponseWriter, r *http.Request) {
 	events, err := a.bot.ParseRequest(r)
 	if err != nil {
-		log.Println(err)
+		log.Printf("parse request error: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	for _, event := range events {
-		if event.Type != linebot.EventTypeMessage {
-			log.Printf("not message event: %v", event)
-			continue
-		}
 		if event.Source.Type != linebot.EventSourceTypeUser {
 			log.Printf("not from user: %v", event)
 			continue
 		}
-		if message, ok := event.Message.(*linebot.TextMessage); ok {
-			log.Printf("text message from %s: %v", event.Source.UserID, message.Text)
-			inferences, err := inferences.BulkFetch(event.Source.UserID)
-			if err != nil {
-				log.Println(err)
-				continue
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			if message, ok := event.Message.(*linebot.TextMessage); ok {
+				log.Printf("text message from %s: %v", event.Source.UserID, message.Text)
+				if err := a.sendCarousel(event.Source.UserID, event.ReplyToken); err != nil {
+					log.Printf("send error: %v", err)
+				}
 			}
-			if len(inferences) < 1 {
-				log.Println("empty inferences")
-				continue
-			}
-			ids := rand.Perm(len(inferences))
-			num := 5
-			if len(ids) < num {
-				num = len(ids)
-			}
-			columns := make([]*linebot.CarouselColumn, 0, 5)
-			for i := 0; i < num; i++ {
-				inference := inferences[ids[i]]
-				columns = append(
-					columns,
-					linebot.NewCarouselColumn(
-						inference.Face.ImageURL,
-						fmt.Sprintf("id: %d", inference.Face.ID),
-						fmt.Sprintf("%s [%f]", inference.Label.Name, inference.Score),
-						linebot.NewURITemplateAction(
-							"ソースを見る",
-							inference.Face.Photo.SourceURL,
-						),
-						linebot.NewPostbackTemplateAction("あってる", "data", ""),
-					),
-				)
-			}
-			_, err = a.bot.ReplyMessage(
-				event.ReplyToken,
-				linebot.NewTemplateMessage(
-					"template message",
-					linebot.NewCarouselTemplate(columns...),
-				),
-			).Do()
-			if err != nil {
-				log.Println(err)
-				continue
-			}
+		case linebot.EventTypePostback:
+			log.Printf("got postback: %s", event.Postback.Data)
+		default:
+			log.Printf("not message or postback event: %v", event)
+			continue
 		}
 	}
+}
+
+func (a *app) sendCarousel(userID, replyToken string) error {
+	inferences, err := inferences.BulkFetch(userID)
+	if err != nil {
+		return err
+	}
+	if len(inferences) < 1 {
+		return errors.New("empty inferences")
+	}
+	ids := rand.Perm(len(inferences))
+	num := 5
+	if len(ids) < num {
+		num = len(ids)
+	}
+	columns := make([]*linebot.CarouselColumn, 0, 5)
+	for i := 0; i < num; i++ {
+		inference := inferences[ids[i]]
+		name := inference.Label.Name
+		if inference.Label.Description != "" {
+			name += " - " + inference.Label.Description
+		}
+		columns = append(
+			columns,
+			linebot.NewCarouselColumn(
+				inference.Face.ImageURL,
+				fmt.Sprintf("id: %d [%.4f]", inference.Face.ID, inference.Score),
+				name,
+				linebot.NewURITemplateAction("くわしく", inference.Face.Photo.SourceURL),
+				linebot.NewPostbackTemplateAction("あってる", string(inference.ID), ""),
+			),
+		)
+	}
+	if _, err = a.bot.ReplyMessage(
+		replyToken,
+		linebot.NewTemplateMessage("template message", linebot.NewCarouselTemplate(columns...)),
+	).Do(); err != nil {
+		return err
+	}
+	return nil
 }
